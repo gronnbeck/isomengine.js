d65e1b6ba1fbf8ab8ccbea5b711ace2dff4b62f4
Ken Grønnbeck <kengroenn@gmail.com>
2012-04-14 14:37:05 -0700

    automatically add layers. Style missing

--- modules/layering.js
+++ modules/layering.js
@@ -1,4 +1,19 @@
 define(function (require) {
-	$ = require('lib/jquery');
-	return {};
+	var $ = require('jquery');
+	return function () {
+		var NO_LAYERS = 3;
+		var layersHtml = "";
+		for (var i = 0; i < NO_LAYERS; i++) {
+			layersHtml +=  "<canvas id='layer-" + i + 
+				"' width='800' height='600'></canvas>"; 
+		}
+     	$('body').html(layersHtml);
+		// TODO: Must fix that the CSS/style is also appended (z-index)
+		var	layers = [];
+		for (var i = 0; i < NO_LAYERS; i ++) {
+			var canvas = document.getElementById("layer-" + i);
+			layers.push(canvas.getContext("2d"));
+		}	
+		return layers;
+	};
 });
