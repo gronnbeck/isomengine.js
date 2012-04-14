96bee189a8228f10395c655d2dcb856fd68abac5
Ken Grønnbeck <kengroenn@gmail.com>
2012-04-14 10:40:22 -0700

    fixed inv_gravity jumping bug

--- modules/forces.js
+++ modules/forces.js
@@ -5,14 +5,16 @@ define(function() {
 			object.speed.z -= 0.3;
 		};
 		this.inv_gravity = function (object) {
-			object.speed.z += 0.3;
-			if (object.speed.z < 0) {
+			if (Math.round(object.speed.z) <= 0) {
 				object.speed.z = 0;
 				// a hack to fix the rounding errors. 
 				// So the object lands on a 
 				// "whole" value of z. Look into this later
 				object.pos.z = Math.round(object.pos.z);
+				return;
 			}
+			object.speed.z += 0.5;
+
 		};
 	};
 	return new forces;
