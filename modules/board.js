be8781fa8f47708d0603b196ca7f8f8f3646c6a9
Ken Grønnbeck <kengroenn@gmail.com>
2012-04-29 12:45:28 -0700

    added class inheritance for board

--- modules/board.js
+++ modules/board.js
@@ -4,40 +4,43 @@ define(function(require) {
 	var TILE_SIZE = 10;
 	var TILE_HEIGHT = TILE_SIZE; // Assuming Cube
 	
-	var board = {};
-	board.DIMENSIONS = { 'x': 30, 'y': 30, 'z': 15 };
-	board.board = {};
-	board.board = new Array(board.DIMENSIONS.x);
-	for (var i = 0; i < board.DIMENSIONS.x; i++) {
-		board.board[i] = new Array(board.DIMENSIONS.y);
-		for (var j = 0; j < board.DIMENSIONS.y; j ++) {
-			board.board[i][j] = new Array(board.DIMENSIONS.z);
-			for (var k = 0; k < board.DIMENSIONS.z; k++) {
-				board.board[i][j][k] = 0;
+	var Board = function() {
+		this.init = function() {
+			this.DIMENSIONS = { 'x': 30, 'y': 30, 'z': 15 };
+			this.board = {};
+			this.board = new Array(this.DIMENSIONS.x);
+			for (var i = 0; i < this.DIMENSIONS.x; i++) {
+				this.board[i] = new Array(this.DIMENSIONS.y);
+				for (var j = 0; j < this.DIMENSIONS.y; j ++) {
+					this.board[i][j] = new Array(this.DIMENSIONS.z);
+					for (var k = 0; k < this.DIMENSIONS.z; k++) {
+						this.board[i][j][k] = 0;
+					};
+				};
 			};
 		};
-	};
 
-	board.draw = function(layers) {
-		c = new brick(layers[0]);
-		c.strokeStyle = "#563500";
-		c.topStyle = "#888";
-		c.leftStyle = "#3d2602";
-		c.rightStyle = c.leftStyle;
-		c.topLayer = layers[1];
+		this.draw = function(layers) {
+			c = new brick(layers[0]);
+			c.strokeStyle = "#563500";
+			c.topStyle = "#888";
+			c.leftStyle = "#3d2602";
+			c.rightStyle = c.leftStyle;
+			c.topLayer = layers[1];
 
-		for (var i = 0; i < board.board.length; i++) {
-			for (var j = 0; j < board.board[i].length; j++) { 
-				for (var k = 0; k < board.board[i][j].length; k++) {		
-					if (board.board[i][j][k] == 0) {
-						continue;
-					}
-					c.draw(TILE_SIZE*(i + 1) + TILE_SIZE*j,  
-						TILE_SIZE*i*0.5 - TILE_SIZE*0.5*j + (300 - TILE_HEIGHT*k));	
+			for (var i = 0; i < this.board.length; i++) {
+				for (var j = 0; j < this.board[i].length; j++) { 
+					for (var k = 0; k < this.board[i][j].length; k++) {		
+						if (this.board[i][j][k] == 0) {
+							continue;
+						}
+						c.draw(TILE_SIZE*(i + 1) + TILE_SIZE*j,  
+							TILE_SIZE*i*0.5 - TILE_SIZE*0.5*j + (300 - TILE_HEIGHT*k));	
+					};
 				};
 			};
 		};
 	};
 
-	return board;
+	return Class.extend(new Board());
 });
